350c069ba8a303e14758c679518ff8f82b83b46e
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-03-24 15:07:46 +0100

    Update license header

--- core/src/main/java/com/github/legman/EventBus.java
+++ core/src/main/java/com/github/legman/EventBus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 The Guava Authors and Sebastian Sdorra
+ * Copyright (C) 2007 The Guava Authors and SCM-Manager Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- core/src/main/java/com/github/legman/ExecutorDecoratorFactory.java
+++ core/src/main/java/com/github/legman/ExecutorDecoratorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Sebastian Sdorra
+ * Copyright (C) 2013 SCM-Manager Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
